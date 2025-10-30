ad1586e566c7e75c34055d69d61cb55d8b359c62
Parikh Pranay Himanshubhai <pranayparikh07@gmail.com>
2025-10-30 19:50:34 +0530

    Create RelativeLayoutActivity.java

---
+++ RelativeLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.example.layoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
