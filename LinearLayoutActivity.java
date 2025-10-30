301e941d4ee0b061fe8b4a51166aeae55bcf633e
Parikh Pranay Himanshubhai <pranayparikh07@gmail.com>
2025-10-30 19:49:58 +0530

    Add LinearLayoutActivity with basic setup

---
+++ LinearLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.example.layoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
